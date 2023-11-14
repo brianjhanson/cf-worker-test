7da4ec9011202cb2c8109b9a04488eccf479af61
Brian Hanson <brian@brianhanson.net>
2023-11-14 09:56:36 -0600

    Match JS

--- php/web/index.php
+++ php/web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = 'http://127.0.0.1:8787/verify';
+$host = 'http://127.0.0.1:8787';
 
 $result = '';
 $signature = '';
@@ -11,15 +11,14 @@ $path = $_POST['path'] ?? '/relieved-raven-af417dbb-local/ap-ds-01.png';
 $width = $_POST['width'] ?? '300';
 $height = $_POST['height'] ?? '200';
 
-if ($_POST) {
-    $path = parse_url($path, PHP_URL_PATH);
-    $params = array_filter([
-        'width' => $width,
-        'height' => $height
-    ]);
+$prefix = '/generate/';
+$url = $_SERVER['REQUEST_URI'];
+$pathname = parse_url($url, PHP_URL_PATH);
+$query = parse_url($url, PHP_URL_QUERY);
 
-    $paramString = http_build_query($params);
-    $data = $path . '#?' . $paramString;
+if (str_starts_with($pathname, $prefix)) {
+    $pathname = '/verify/' .substr($pathname, strlen($prefix));
+    $data = $pathname . '#?' . $query;
 
     $signature = base64_encode(hash_hmac(
         'sha256',
@@ -27,106 +26,6 @@ if ($_POST) {
         $signing_key
     ));
 
-    $result = $host . $path . '?' . $paramString . '&s=' . $signature;
+    $verifyUrl = $host . $pathname . '?'. $query . '&s=' . $signature;
+    echo '<a target="_blank" href="' . $verifyUrl .'">' . $verifyUrl . '</a>';
 }
-?>
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta
-            name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-        >
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <style>
-          :root {
-            font-family: sans-serif;
-            box-sizing: border-box;
-          }
-
-          .app {
-            display: grid;
-            grid-template-columns: 1fr [content-start] min(80%, 800px) [content-end] 1fr;
-          }
-
-          .app > * {
-            grid-column: content;
-          }
-
-          label {
-            display: block;
-          }
-
-          input {
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            padding: 0.5em;
-            width: 100%;
-            max-width: 600px;
-          }
-
-          .stack > * + * {
-            margin-top: 1rem;
-
-          }
-
-          button {
-            padding: 0.5em 1em;
-            margin-top: 2em;
-            border-radius: 3px;
-            appearance: none;
-            background-color: cornflowerblue;
-            border: 1px solid transparent;
-            font-family: inherit;
-            color: #fff;
-            font-size: 0.875rem;
-          }
-
-          pre {
-            background-color: #e8e8e8;
-            padding: 1rem;
-            border-radius: 4px;
-          }
-
-        </style>
-    </head>
-    <body>
-        <div class="app">
-
-            <h1 class="text-xl font-bold">Request signing</h1>
-
-            <form action="/" method="POST">
-                <div class="stack">
-
-                    <div>
-                        <label for="path">Path</label>
-                        <input type="text" name="path" value="<?php echo $path; ?>" />
-                    </div>
-                    <div>
-                        <label for="width">Width</label>
-                        <input type="text" name="width" value="<?php echo $width; ?>" />
-                    </div>
-                    <div>
-                        <label for="height">Height</label>
-                        <input type="text" name="height" value="<?php echo $height; ?>" />
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <h2>Test URL</h2>
-                        <a href="<?php echo $result; ?>" target="_blank"><?php echo $result; ?></a>
-                    </div>
-                    <pre><?php echo $data; ?></pre>
-                    <pre><?php echo $signature; ?></pre>
-                    <pre><?php echo base64_decode($signature); ?></pre>
-                    <input type="text">
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    </body>
-</html>
